2687c6d0d6506a2365479b2767be7e1cedcc0a9f
SiZiOUS <sizious@gmail.com>
2020-06-28 21:13:56 +0200

    Initial commit of dcdis 0.4a

---
+++ src/decode.h
@@ -0,0 +1,19 @@
+/* decode.h
+ *
+ * Copyright (C) 1999 Lars Olsson
+ *
+ * This file is part of dcdis.
+ *
+ * dcdis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+#ifndef _DECODE_H_
+#define _DECODE_H_
+
+#include <sys/types.h>
+
+char *decode(uint16_t, uint32_t, char *, uint32_t, uint32_t);
+
+#endif
